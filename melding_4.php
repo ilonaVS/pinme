3c1f7ffedfe5367c105bfc446ccadc15d4574d4a
ilonaVS <37265476+ilonaVS@users.noreply.github.com>
2018-04-23 21:43:38 +0200

    Meldingspagina's basic html

---
+++ melding_4.php
@@ -0,0 +1,14 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width">
+    <link rel="stylesheet" type="text/css" href="style/reset.css">
+    <link rel="stylesheet" type="text/css" href="style/style.css">
+    
+    <title>PinME - Melding toevoegen</title>
+</head>
+<body>
+
+
+</body>
+</html>
