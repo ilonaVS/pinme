e5358c3f248ea6a00969c8b8e67121b6dcaed371
ilonaVS <37265476+ilonaVS@users.noreply.github.com>
2018-04-24 22:40:04 +0200

    Google Maps API

---
+++ js/googleMaps.js
@@ -0,0 +1,7 @@
+function myMap() {
+var mapProp= {
+    center:new google.maps.LatLng(51.024902, 4.479083),
+    zoom:15,
+};
+var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
+}
--- melding_1.php
+++ melding_1.php
@@ -1,14 +1,20 @@
+<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width">
-    <link rel="stylesheet" type="text/css" href="style/reset.css">
-    <link rel="stylesheet" type="text/css" href="style/style.css">
+    <link rel="stylesheet" type="text/css" href="css/reset.css">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
     
     <title>PinME - Melding toevoegen</title>
 </head>
 <body>
+<?php include_once("nav.inc.php"); ?>
 
+<div id="googleMap"></div>
+<script src="js/googleMaps.js"></script>
+<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCEovYLDDYFLJ6SCyDn-lxjl3N2WHM27DI&callback=myMap"></script>
 
 </body>
 </html>
