d82c019f1709b8ad7d7052827098f35379454e37
Anne <annegeerkens@gmail.comgit credential-osxkeychaingit credential-osxkeychaincd ..cd desktopAnne:/ annegeerkens$ git config --global user.name AnneAnne:/ annegeerkens$ git config --global user.email annegeerkens@gmail.com>
2018-04-24 14:21:37 +0200

    css aangevuld met img en al een hoofscherm

---
+++ hoofdscherm.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Welkom</title>
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+	<link rel="stylesheet" href="css/flexboxgrid.css">
+	<link rel="stylesheet" href="css/reset.css">
+	<link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+
+<div id="container" class="hoofdscherm">
+	
+	<div id="logo"></div>
+	
+	<div class="knop">Registreren</div>
+	
+	<p>Heeft u reeds een account? Log in.</p>
+	
+	
+</div>
+	
+</body>
+</html>
