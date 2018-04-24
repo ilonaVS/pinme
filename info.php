7bacf34093635b17e09915a0f4d99ff62147fea7
Anne <annegeerkens@gmail.comgit credential-osxkeychaingit credential-osxkeychaincd ..cd desktopAnne:/ annegeerkens$ git config --global user.name AnneAnne:/ annegeerkens$ git config --global user.email annegeerkens@gmail.com>
2018-04-24 14:44:32 +0200

    info en registratie pagina aangemaakt en al wat css aangegeven

---
+++ info.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Info</title>
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+	<link rel="stylesheet" href="css/flexboxgrid.css">
+	<link rel="stylesheet" href="css/reset.css">
+	<link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+
+<div id="container" class="info">
+	
+	<div id="logo"></div>
+	
+	<div id="tekst">
+	<p>Dit is de meldingsapp van Mechelen.
+Met één pin help je Mechelen tot een betere stad.
+<br><br>
+Bekijk alle meldingen in real-time 
+en raadpleeg jouw persoonlijk dossier.</p>
+	</div>
+	
+</div>
+	
+</body>
+</html>
---
+++ registreren.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Registreren</title>
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+	<link rel="stylesheet" href="css/flexboxgrid.css">
+	<link rel="stylesheet" href="css/reset.css">
+	<link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+
+<div id="container" class="registratie">
+	
+	<div id="logo"></div>
+	
+	<form action="" method="post">
+				<h1>Registreren</h1>
+  
+               
+				<div class="formfield">
+					<input type="text" id="email" name="email" placeholder="E-mail">
+				</div>
+				<div class="formfield">
+					<input type="password" id="password" name="password" placeholder="Wachtwoord">
+				</div>
+
+                		<div class="formfield">
+					<input type="password" id="password_confirmation" name="password_confirmation" placeholder="Bevestiging wachtwoord">
+				</div>
+
+				<div class="formfield">
+					<input type="submit" value="Registreren" class="button">	
+				</div>
+        
+    </form>
+	
+</div>
+	
+</body>
+</html>
+
+
+
+
+
