9389b77674e671c88aeb6bcfec6fc6382d530786
ilonaVS <37265476+ilonaVS@users.noreply.github.com>
2018-05-01 16:46:18 +0200

    included font-family open sans on all pages

--- melding_6.php
+++ melding_6.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width">
     <link rel="stylesheet" type="text/css" href="css/reset.css">
     <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
     
     <title>PinME - Melding toevoegen</title>
 </head>
