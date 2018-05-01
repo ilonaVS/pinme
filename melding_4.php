ca33671f7943a92c5c797d57b307818d5e95bc9a
ilonaVS <37265476+ilonaVS@users.noreply.github.com>
2018-05-01 15:17:59 +0200

    Melding toevoegen - subrubriek kiezen html+css

--- melding_4.php
+++ melding_4.php
@@ -13,5 +13,33 @@
 
 <h2>Melding toevoegen</h2>
 
+<a href="melding_1.php" class="back_btn"><img src="images/pinme_backbtn.png"></a>
+
+<div class="subrub">
+    <img src="images/icon_afval.png" alt="icon">
+
+    <form action="melding_5.php" method="post" enctype="multipart/form-data" id="uploadForm">
+
+        <div class="formfield">  
+            <input type="submit" value="Sluikstorten" name="submit" class="btn_subrub">
+        </div>
+    
+        <div class="formfield">  
+            <input type="submit" value="Zwerfvuil" name="submit" class="btn_subrub">
+        </div>
+    
+        <div class="formfield">  
+            <input type="submit" value="Huisvuil" name="submit" class="btn_subrub">
+        </div>
+    
+        <div class="formfield">  
+            <input type="submit" value="Hondenpoep" name="submit" class="btn_subrub">
+        </div>
+
+        <div class="formfield">  
+            <input type="submit" value="Andere" name="submit" class="btn_subrub">
+        </div>
+    </form>
+</div>
 </body>
 </html>
