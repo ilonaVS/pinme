18d9c6bf2c0db40318ba66fc2a81581142b964ea
ilonaVS <37265476+ilonaVS@users.noreply.github.com>
2018-05-01 17:56:33 +0200

    navigation links added

--- nav.inc.php
+++ nav.inc.php
@@ -1,5 +1,5 @@
 <div class="navbar">
-  <a href="#" id="home_btn" class="nav_btn">Home</a>
-  <a href="#melding_1.php" id="melding_btn" class="nav_btn">Melding</a>
-  <a href="#" id="dossier_btn" class="nav_btn">Dossier</a>
+  <a href="home.php" id="home_btn" class="nav_btn">Home</a>
+  <a href="melding_1.php" id="melding_btn" class="nav_btn">Melding</a>
+  <a href="dossier.php" id="dossier_btn" class="nav_btn">Dossier</a>
 </div>
