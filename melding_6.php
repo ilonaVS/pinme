ba6ca06499098a66d248699ad624dbc3a8c33a4a
ilonaVS <37265476+ilonaVS@users.noreply.github.com>
2018-05-01 16:42:13 +0200

    Melding toegevoegd html

--- melding_6.php
+++ melding_6.php
@@ -3,13 +3,20 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width">
-    <link rel="stylesheet" type="text/css" href="style/reset.css">
-    <link rel="stylesheet" type="text/css" href="style/style.css">
+    <link rel="stylesheet" type="text/css" href="css/reset.css">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
     
     <title>PinME - Melding toevoegen</title>
 </head>
 <body>
 
+<div class="melding_succes">
+   
+    <img src="images/icon_melding_succes.png" alt="melding succes">
+
+    <h3>Melding opgeslagen!</h3>
+    
+</div>
 
 </body>
 </html>
