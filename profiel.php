6333719f89eb2d28965a8832726fdac372831a7b
Anne <annegeerkens@gmail.comgit credential-osxkeychaingit credential-osxkeychaincd ..cd desktopAnne:/ annegeerkens$ git config --global user.name AnneAnne:/ annegeerkens$ git config --global user.email annegeerkens@gmail.com>
2018-05-02 11:11:00 +0200

    profiel pagina

---
+++ profiel.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Profiel</title>
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+	<link rel="stylesheet" href="css/flexboxgrid.css">
+	<link rel="stylesheet" href="css/reset.css">
+	<link rel="stylesheet" href="css/style.css">
+	
+	
+</head>
+
+<body>
+<div id="container" class="profiel">
+	
+<h1>test</h1>
+
+	
+
+	
+</div>
+
+	
+		
+		
+		
+
+</body>
+</html>
