bf4d1b2ec9ec84e0f13d158b27e9b69a90b1da84
ilonaVS <37265476+ilonaVS@users.noreply.github.com>
2018-05-07 22:10:14 +0200

    Home.php - infobalk html + css

--- home.php
+++ home.php
@@ -19,11 +19,19 @@
 <script src="js/googleMaps.js"></script>
 <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCEovYLDDYFLJ6SCyDn-lxjl3N2WHM27DI&callback=myMap"></script>
 
-<div>
-   <img href="">
-    <div># opgeloste meldingen</div>
-    
+<div class="popup_info">
+    <img class="btn_down" src="images/btn_down.png" alt="btn_down">
+    <img class="logo_small" src="images/logo_small.png" alt="logo">
+    <div class="home_info"># aangemaakte pins</div>
+    <div class="home_info"># opgeloste pins</div>
+    <div class="home_info"># gebruikers</div>
 </div>
+
+
+<script>
+    /*Nog toe te voegen*/
+		
+</script>
 
 </body>
 </html>
