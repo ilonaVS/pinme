43b025461140bd8aaacab91f9cbf7a4da3f3e982
ilonaVS <37265476+ilonaVS@users.noreply.github.com>
2018-05-19 18:38:37 +0200

    customized marker on map

--- js/homeGoogleMaps.js
+++ js/homeGoogleMaps.js
@@ -22,6 +22,30 @@ var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
         };
         map.setCenter(pos);
     });
+
+    var iconBase = '../pinme/images/';
+    var locations = [
+	[ 50.995659, 4.530104, 2, 'icon_afval.png'],
+	[ 51.012688, 4.440343, 1, 'icon_groen.png']
+    ];
+    
+
+var marker, i, icon;
+for (i = 0; i < locations.length; i++) {
+    icon = {
+    url: iconBase + locations[i][3],
+    scaledSize: new google.maps.Size(50, 50), // scaled iconsize
+    
+    };
+	marker = new google.maps.Marker({
+		position: new google.maps.LatLng(locations[i][0], locations[i][1]),
+		icon: icon,
+		map: map
+	});
+    
+
+};
+
         
     }     
 
