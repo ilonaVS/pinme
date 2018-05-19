22b803714dd6bd31fb33e1ceffa68047b20dec09
Anne <annegeerkens@gmail.com>
2018-05-19 23:50:03 +0200

    css
    css

--- info.php
+++ info.php
@@ -12,23 +12,26 @@
 </head>
 <body>
 
+ 
+  
 <div class="info">
+  <div id="lijn"></div>
 	
-	<img id="logo" src="images/pinme_logo.png" alt="logo">
+	<img id="logo" src="images/logo_wit.png" alt="logo">
 	
 	<div id="tekst">
 	<p>Dit is de meldingsapp van Mechelen. Met één pin help je Mechelen tot een betere stad.</p>
     <p>Bekijk alle meldingen in real-time en raadpleeg jouw persoonlijk dossier.</p>
 	</div>
 	
-	
-    <div id="informatie">
-        <div class="stap_info"></div>
-	    <div class="stap_info"></div>
-	    <div class="stap_info"></div>
 	</div>
-	
-</div>
-	
+
+   
+    <div id="informatie">
+        Go!
+	</div>
+
+
+
 </body>
 </html>
