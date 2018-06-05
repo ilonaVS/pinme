25a4b1860a11d269b5bb581b5e982920ebbd3576
Anne <annegeerkens@gmail.com>
2018-06-05 12:20:00 +0200

    landingpage

--- landingpage.php
+++ landingpage.php
@@ -1,43 +1,58 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>HOME</title>
-		
-	<link rel="stylesheet" href="css/flexboxgrid.css">
-	<link rel="stylesheet" href="css/reset.css">
-	<link rel="stylesheet" href="css/style.css">
-	<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>HOME</title>
+    <link rel="stylesheet" href="css/flexboxgrid.css">
+    <link rel="stylesheet" href="css/reset.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Fredoka+One|Raleway" rel="stylesheet">
 </head>
+
 <body>
 
+<nav class="test">
+	
+	<ul>
+            <li><a href="#">home</a>
+            </li>
+            <li><a href="#">info</a>
+            </li>
+            <li><a href="#">ontwikkelaars</a>
+            </li>
+            <li><a href="#">contact</a>
+            </li>
+
+        </ul>
+	
+</nav>
+    <div id="container" class="landing">
+
+        <img src="images/test.png" alt="" width="45%">
+        <img id="logo_landing" src="images/pinme_logo.png" alt="logo" width="5%">
+	
+        <div id="tekstlanding">
+
+        <h1>De nieuwe meldingsapp van Mechelen</h1>
+
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima voluptatem blanditiis quis ipsum dolorem amet sunt autem dolor tenetur quod, qui optio similique vel velit iste illum, ducimus aliquam quasi.</p>
 
 
 
-<div id="container" class="landing">
-  
-        <img src="images/test.png" alt="" width="40%">
-        <img id="logo_landing" src="images/logo_wit.png" alt="logo" width="5%">
 
-
-<h1>De nieuwe meldingsapp van Mechelen</h1>
-
-<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima voluptatem blanditiis quis ipsum dolorem amet sunt autem dolor tenetur quod, qui optio similique vel velit iste illum, ducimus aliquam quasi.</p>
-
-
-
-
- <div class="formfield">
-            <input type="submit" value="Download" class="button">	
+      <div class="formfield">
+            <input type="submit" value="Download" class="button">
         </div>
-        
-        
-      
+
 
 </div>
+    </div>
 
 
-	
+
 </body>
+
 </html>
